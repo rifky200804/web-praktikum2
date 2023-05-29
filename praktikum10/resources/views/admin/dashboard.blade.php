f7f20cfba0a52e0a146a89e7db3f7e88486a39da
rifky200804 <rifkysyiahbudin200804@gmail.com>
2023-05-29 22:05:26 +0700

    Praktikum 10 - Muhammad Rifky Syiahbudin 0110222066

---
+++ praktikum10/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.layout.appadmin')
+@section('title','Dashboard Admin')
+@section('content')
+    Ini adalah dashboard
+@endsection
---
+++ praktikum10/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProdukController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
+
+Route::get('/produk', [ProdukController::class, 'index']);
+
+Route::get('/home', function(){return view('front.home');});
+Route::get('/about', function(){return view('front.about');});
+Route::get('/contact', function(){return view('front.contact');});
+
